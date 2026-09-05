93eb5de22f8012d44b3c8289316da4d34685b468
xt9y <felix@xt9y.de>
2026-09-06 00:43:30 +0200

    fix platform C++ runtime linkage

--- build.c
+++ build.c
@@ -10,12 +10,14 @@ static void configurePlatform(C_Target *target)
     c_framework(target, "Cocoa");
     c_framework(target, "IOKit");
     c_framework(target, "CoreVideo");
+    c_link_system(target, "c++");
 #else
     c_link_system(target, "GL");
     c_link_system(target, "GLU");
     c_link_system(target, "m");
     c_link_system(target, "dl");
     c_link_system(target, "pthread");
+    c_link_system(target, "stdc++");
 #endif
     c_link_system(target, "glfw");
 }
@@ -43,7 +45,6 @@ void build(C_Build *b)
     c_link_flag(game, "-L/usr/local/lib");
     c_link_flag(game, "-llwcgl-2.9.3");
     c_link_flag(game, "-Wl,-rpath,/usr/local/lib");
-    c_link_system(game, "stdc++");
 
     c_default_target(b, game);
 }
